ca8efc5ba692ca4f76ab48885520308592a52e7f
Jordan Hewitt <hewitj2@gmail.com>
2020-03-31 18:34:43 -0700

    migrated static site to laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="/css/animate.css">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    </head>
+    <body>
+
+  <section id="welcome">
+    <div class="welcome-menu">
+      <div class="logo">
+        <img src="/img/logo.png">
+      </div>
+      <div class="menu">
+
+        <div class="menu-title">
+          Menu
+        </div>
+
+        <ul class="links">
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Waitlist</a>
+          </li>
+          <li>
+            <a href="#">Offers</a>
+          </li>
+          <li>
+            <a href="#">Gift Cards</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="social-icons">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+      </div>
+      <div class="location">
+        <div class="address">
+          234 Main St,<br>
+          Los Angeles, CA 90069
+        </div>
+        <div class="phone">
+          <a href="tel:+13106038967">310-603-8967</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="welcome-jumbo">
+      <div class="status animated bounceInUp">
+        new
+      </div>
+      <h1 class="animated bounceInUp">Double Down Burger</h1>
+      <img src="/img/burger.png" class="burger-fries animated bounceInUp">
+    </div>
+
+  </section>
+
+  <section id="information">
+
+    <div class="restaurant-image">
+      <img src="/img/cafe2.jpg">
+    </div>
+
+    <div class="info">
+
+      <h2>We started with two cows and a dream in 1989.</h2>
+
+      <div class="paragraphs">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+          aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+          aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+
+      <a href="#" class="about-link">
+        <span>Learn more about our restaurant</span>
+        <div class="circle-right"><i class="fa fa-chevron-right"></i></div>
+      </a>
+
+  </div>
+
+    </div>
+
+  </section>
+
+  <section id="food-preview">
+
+    <h2>We have everything you need to kill your hunger</h2>
+
+    <div class="button-rounded">View Our Menu</div>
+
+    <div class="food-slider">
+
+      <div class="slide">
+        <div class="background"></div>
+        <div class="content">
+          <div class="food-title">
+            Starters
+          </div>
+          <p class="description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <img src="/img/nachos.png">
+        </div>
+      </div>
+
+      <div class="slide">
+        <div class="background"></div>
+        <div class="content">
+          <div class="food-title">
+            Burgers
+          </div>
+          <p class="description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <img src="/img/burger.png">
+        </div>
+      </div>
+
+      <div class="slide">
+        <div class="background"></div>
+        <div class="content">
+          <div class="food-title">
+            Entrees
+          </div>
+          <p class="description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <img src="/img/entrees.png">
+        </div>
+      </div>
+
+      <div class="slide">
+        <div class="background"></div>
+        <div class="content">
+          <div class="food-title">
+            Sides
+          </div>
+          <p class="description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <img src="/img/salad.png">
+        </div>
+      </div>
+
+      <div class="slide">
+        <div class="background"></div>
+        <div class="content">
+          <div class="food-title">
+            Desserts
+          </div>
+          <p class="description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <img src="/img/dessert.png">
+        </div>
+      </div>
+
+
+    </div>
+
+    </div>
+
+  </section>
+
+  <div>
+
+  </div>
+
+</body>
+
+</html>
