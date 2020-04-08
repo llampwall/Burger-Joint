331a75ca5a59e7e606a977e396ee08111641e398
Jordan Hewitt <hewitj2@gmail.com>
2020-04-07 18:06:14 -0700

    fixed display and price issues in single-menu

--- resources/views/menu/single-menu.blade.php
+++ resources/views/menu/single-menu.blade.php
@@ -7,28 +7,29 @@
 @section('content')
     <div id="single-menu">
         <div class="row">
-            <div class="col-lg-8 offset-lg-2 col-md-12">
+            <div class="col-lg-10 offset-lg-1 col-md-12">
                 <div class="content-box">
-                    <div class="row no-gutters">
+                    <div class="row">
                         <div class="col-md-12">
                             <h1>{{$category->title}}</h1>
                         </div>
 
                         @foreach ($items as $item)
-                        <div class="col-lg-4">
+                        <div class="col-lg-6">
                             <div class="item">
                                 <div class="title">
                                     <h4>{{$item->title}}</h4>
-                                    <span class="price"{{$item->price}}</span>
+                                    <span class="price">{{$item->price}}</span>
                                 </div>
                                 <div class="description">
                                     <p>{{$item->description}}</p>
                                 </div>
                             </div>
+                            <div class="image">
+                                <img src="{{$item->image_url}}"/>
+                            </div>
                         </div>
-                        <div class="image col-lg-2 align-self-end">
-                            <img src="{{$item->image_url}}"/>
-                        </div>
+                        
                         @endforeach
                     </div>
                     <a href="/menu" class="button-rounded">Back to Full Menu</a>
